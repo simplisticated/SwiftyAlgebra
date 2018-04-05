c9c881a5da1f27b1165b9a71933f08d043d28bd0
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-04-05 23:06:37 +0300

    Added demo project

---
+++ SwiftyAlgebra/SwiftyAlgebra/SwiftyAlgebra/SwiftyAlgebra.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyAlgebra.h
+//  SwiftyAlgebra
+//
+//  Created by Igor Matyushkin on 05.04.2018.
+//  Copyright © 2018 Igor Matyushkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyAlgebra.
+FOUNDATION_EXPORT double SwiftyAlgebraVersionNumber;
+
+//! Project version string for SwiftyAlgebra.
+FOUNDATION_EXPORT const unsigned char SwiftyAlgebraVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyAlgebra/PublicHeader.h>
+
+
---
+++ SwiftyAlgebra/SwiftyAlgebraDemo/SwiftyAlgebraDemo/ViewControllers/Main/MainViewController.swift
@@ -0,0 +1,52 @@
+//
+//  MainViewController.swift
+//  SwiftyAlgebraDemo
+//
+//  Created by Igor Matyushkin on 05.04.2018.
+//  Copyright © 2018 Igor Matyushkin. All rights reserved.
+//
+
+import UIKit
+import SwiftyAlgebra
+
+class MainViewController: UIViewController {
+    
+    // MARK: Class variables & properties
+    
+    // MARK: Public class methods
+    
+    // MARK: Private class methods
+    
+    // MARK: Initializers
+    
+    // MARK: Deinitializer
+    
+    deinit {
+    }
+    
+    // MARK: Outlets
+    
+    // MARK: Object variables & properties
+    
+    // MARK: Public object methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Initialize navigation bar
+        
+        self.navigationItem.title = "SwiftyAlgebra Demo"
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: Private object methods
+    
+    // MARK: Actions
+    
+    // MARK: Protocol implementation
+    
+}
