4c1d88fd9b088db5426e3ab0d90fa6ba4bbe285a
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-04-26 01:21:34 +0300

    Created Progressable protocol

---
+++ Source/Engine/Progressable.swift
@@ -0,0 +1,15 @@
+//
+//  Progressable.swift
+//  SwiftyAlgebra
+//
+//  Created by Igor Matyushkin on 26.04.2018.
+//  Copyright © 2018 Igor Matyushkin. All rights reserved.
+//
+
+import Foundation
+
+public protocol Progressable {
+    
+    var progress: Float { get }
+    
+}
