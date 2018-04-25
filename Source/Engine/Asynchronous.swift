1d356502af6ddc1b3dd0fc0d22ea89c72c277b92
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-04-26 01:21:45 +0300

    Added Asynchronous protocol

---
+++ Source/Engine/Asynchronous.swift
@@ -0,0 +1,12 @@
+//
+//  Asynchronous.swift
+//  SwiftyAlgebra
+//
+//  Created by Igor Matyushkin on 26.04.2018.
+//  Copyright © 2018 Igor Matyushkin. All rights reserved.
+//
+
+import Foundation
+
+public protocol Asynchronous: Startable, Progressable, Cancellable {
+}
